7e0ac664f08cc10a45f51c00008c2847d317a7b2
magaiver <magaiver.lsilva@gmail.com>
2026-02-06 18:20:25 -0300

    criação de sessão totem

--- app/test-fully/page.tsx
+++ app/test-fully/page.tsx
@@ -1,14 +1,26 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function TestFully() {
+  const [info, setInfo] = useState<any>({})
+
   useEffect(() => {
     // @ts-ignore
-    console.log('fully:', (window as any).fully)
+    const fullyObj = (window as any).fully
     // @ts-ignore
-    console.log('deviceId:', (window as any).fully?.getDeviceId?.())
+    const deviceId = fullyObj?.getDeviceId?.()
+
+    setInfo({
+      fullyExists: !!fullyObj,
+      deviceId: deviceId || 'NÃO ENCONTRADO'
+    })
   }, [])
 
-  return <div>Veja o console</div>
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Teste Fully</h1>
+      <pre>{JSON.stringify(info, null, 2)}</pre>
+    </div>
+  )
 }
